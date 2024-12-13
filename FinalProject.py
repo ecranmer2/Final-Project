8a0be28041583d234c44e44669174d90f2ca1db5
Emma Cranmer <emmacranmr00@gmail.com>
2024-12-13 15:29:30 -0700

    change p3

--- FinalProject.py
+++ FinalProject.py
@@ -1,15 +1,15 @@
-def li_app (income, education, parent, married, female, age):
+# load packages
+import pandas as pd
+import numpy as np
+import altair as alt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import streamlit as st
+import joblib
 
-    #load packages
-    import pandas as pd
-    import numpy as np
-    import altair as alt
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import classification_report
-    from sklearn.metrics import confusion_matrix
-    import streamlit as st
-    import joblib
+def li_app (income, education, parent, married, female, age):
 
     #create person
     person = [
