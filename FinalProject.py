8c0f7e75c8428156d87acd41b7b1c44306936f50
Emma Cranmer <emmacranmr00@gmail.com>
2024-12-13 16:34:45 -0700

    c

--- FinalProject.py
+++ FinalProject.py
@@ -1,7 +1,11 @@
 # load packages
 import pandas as pd
 import numpy as np
+import altair as alt
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 import streamlit as st
 import joblib
 
